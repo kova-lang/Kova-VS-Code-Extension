dab8b9e912cccf4e779fa709f44bd5316c25e8ef
cybugg <timehinsamuel11@gmail.com>
2026-03-27 17:26:54 +0100

    add: vs code launch, did some few clean ups ,icon now works. Currrntly still working on kova manual, so might take some time to fully develop the whole extension

---
+++ out/extension.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function activate(context) {
+    console.log("Kova extension is now active");
+}
+function deactivate() { }
+module.exports = {
+    activate,
+    deactivate
+};
+//# sourceMappingURL=extension.js.map
