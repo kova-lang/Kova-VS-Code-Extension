bebffb52120e168d74c4bd95f2d5b0d118600eda
cybugg <timehinsamuel11@gmail.com>
2026-03-27 17:36:54 +0100

    feat: update the ./icons/kova-icon-theme.json to inherit prvious icons and only extend it with kova

--- out/extension.js
+++ out/extension.js
@@ -1,11 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function activate(context) {
-    console.log("Kova extension is now active");
-}
-function deactivate() { }
-module.exports = {
-    activate,
-    deactivate
-};
 //# sourceMappingURL=extension.js.map
